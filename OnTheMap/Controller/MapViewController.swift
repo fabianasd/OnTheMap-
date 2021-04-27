084e7d08d9566fa44b8b8d895bf0ab377b286d01
fabianasd <fafadiaz@hotmail.com>
2021-04-26 23:10:47 -0300

    Criação das telas Map e List

---
+++ OnTheMap/Controller/MapViewController.swift
@@ -0,0 +1,12 @@
+//
+//  OnTheMapViewController.swift
+//  OnTheMap
+//
+//  Created by Fabiana Petrovick on 26/04/21.
+//  Copyright © 2021 Fabiana Petrovick. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+}
---
+++ OnTheMap/Controller/MapViewControllerList.swift
@@ -0,0 +1,13 @@
+//
+//  OnTheMapViewControllerList.swift
+//  OnTheMap
+//
+//  Created by Gabriel Petrovick on 26/04/21.
+//  Copyright © 2021 Fabiana Petrovick. All rights reserved.
+//
+
+import UIKit
+
+
+class MapViewControllerList: UIViewController {
+}
