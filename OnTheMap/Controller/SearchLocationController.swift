4be0b37fb21db7cda46daf67ae364102169eefa8
fabianasd <fafadiaz@hotmail.com>
2021-05-09 22:36:27 -0300

    feat: searchLocation

--- OnTheMap/Controller/SearchLocationController.swift
+++ OnTheMap/Controller/SearchLocationController.swift
@@ -8,7 +8,36 @@
 
 import UIKit
 
-class SearchLocationController: UIViewController {   
+class SearchLocationController: UIViewController {
+    /*
+     ao cancelar deve voltar para tela anterior editingLocation (confirmar tela)
+     informar link do linkedin no textField
+     mostrar um preview no map (confirmar)
+     ao clicar em submit cadastrar o link junto com nome e localizacao
+     */
+    @IBOutlet weak var linkLinkedin: UITextField!
+    @IBOutlet weak var cancel: UIBarButtonItem!
+    @IBOutlet weak var submit: UIButton!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        linkLinkedin.text = "Enter a Link to Share Here"
+    }
+    
+    @IBAction func linkLinkedinTextField(_ sender: UITextField) {
+        sender.text = ""
+    }
+    
+    
+    @IBAction func submit(_ sender: Any) {
+        //deve salvar no MapModel o link do linkedin, juntamente com a localizacao e nome do usuario
+        //retornar as informacoes no mapViewControllerList
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        DispatchQueue.main.async {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
 }
 
